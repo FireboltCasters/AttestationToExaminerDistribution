0bab63f320a1b76747d7ba50a7362136384a852f
Nils <nilsbaumgartner1994@gmail.com>
2022-10-27 20:23:22 +0200

    Added Export

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -3,5 +3,12 @@
 import PrecedenceGraph from "./ignoreCoverage/PrecedenceGraph";
 import Example from "./Example";
 
+import ParseStudIPCSV from "./ignoreCoverage/ParseStudIPCSV";
+import ExampleCSVContent from "./ignoreCoverage/ExampleCSVContent";
+import GraphHelper from "./ignoreCoverage/GraphHelper";
+
 export {PrecedenceGraph}
 export { Example };
+export { ParseStudIPCSV };
+export { ExampleCSVContent };
+export { GraphHelper };
